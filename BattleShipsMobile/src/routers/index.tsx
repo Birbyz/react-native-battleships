10d466631504218509d1e0476828854bd98846bb
Birbyz <imbirbyz@gmail.com>
2024-04-03 19:44:06 +0300

    fix login and register redirection

--- BattleShipsMobile/src/routers/index.tsx
+++ BattleShipsMobile/src/routers/index.tsx
@@ -5,11 +5,11 @@ import { useContext } from "react";
 import { AppContext } from "../context/app.context";
 
 const Router: React.FC = () => {
-  const { id } = useContext<any>(AppContext)
+  const { id, jwt } = useContext<any>(AppContext)
 
   return (
     <NavigationContainer>
-      {id ? gameRouter : authRouter}
+      {(id || jwt) ? gameRouter : authRouter}
     </NavigationContainer>
   );
 };
